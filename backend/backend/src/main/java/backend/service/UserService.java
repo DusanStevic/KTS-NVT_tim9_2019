0f3ee806d05b0fa9d5dc9446126c252de25cdc07
Dusan Stevic <stevicdule@gmail.com>
2019-11-06 03:37:29 +0100

    zavrsena registracija usera + provereno postmanom

--- backend/backend/src/main/java/backend/service/UserService.java
+++ backend/backend/src/main/java/backend/service/UserService.java
@@ -2,6 +2,9 @@ package backend.service;
 
 import java.util.List;
 
+import backend.dto.RegistrationDTO;
+import backend.model.Administrator;
+import backend.model.RegisteredUser;
 import backend.model.User;
 
 
@@ -10,4 +13,6 @@ public interface UserService {
     User findById(Long id);
     User findByUsername(String username);
     List<User> findAll ();
+    RegisteredUser registerUser(RegistrationDTO registrationDTO);
+    Administrator registerAdmin(RegistrationDTO registrationDTO);
 }
--- backend/backend/src/main/java/backend/service/impl/UserServiceImpl.java
+++ backend/backend/src/main/java/backend/service/impl/UserServiceImpl.java
@@ -1,21 +1,35 @@
 package backend.service.impl;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
+import backend.dto.RegistrationDTO;
+import backend.model.Administrator;
+import backend.model.Authority;
+import backend.model.RegisteredUser;
+import backend.model.Role;
 import backend.model.User;
 import backend.repository.UserRepository;
 import backend.service.UserService;
-
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	
 
 	@Override
 	public User findByUsername(String username) throws UsernameNotFoundException {
@@ -35,5 +49,37 @@ public class UserServiceImpl implements UserService {
 	
 	public User save(User user) {
 		return userRepository.save(user);
+	}
+
+	@Override
+	public RegisteredUser registerUser(RegistrationDTO registrationDTO) {
+		RegisteredUser registeredUser = new RegisteredUser();
+		registeredUser.setUsername(registrationDTO.getUsername());
+		registeredUser.setPassword(passwordEncoder.encode(registrationDTO.getPassword()));
+		//account je aktiviran
+		registeredUser.setEnabled(true);
+		//prvi put se prijavljuje na sistem
+		registeredUser.setFirstTime(true);
+		registeredUser.setLastPasswordResetDate(new Timestamp(System.currentTimeMillis()));
+		registeredUser.setFirstName(registrationDTO.getFirstName());
+		registeredUser.setLastName(registrationDTO.getLastName());
+		registeredUser.setEmail(registrationDTO.getEmail());
+		List<Authority> authorities = new ArrayList<>();
+		Authority a = new Authority();
+		a.setRole(Role.ROLE_REGISTERED_USER);
+		authorities.add(a);
+		registeredUser.setAuthorities(authorities);
+		
+		userRepository.save(registeredUser);
+		return registeredUser;
+	}
+
+	@Override
+	public Administrator registerAdmin(RegistrationDTO registrationDTO) {
+		Administrator administrator = new Administrator();
+		
+		
+		userRepository.save(administrator);
+		return administrator;
 	}
 }
