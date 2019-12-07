d4f0682c0f1682734cda1165ff5a1291d7ef1462
Arpad Varga Šomođi <arpad.varga.somodji@gmail.com>
2019-12-07 17:44:22 +0100

    Added junit 4

--- backend/backend/src/test/java/backend/service/UserServiceIntegrationTest.java
+++ backend/backend/src/test/java/backend/service/UserServiceIntegrationTest.java
@@ -1,5 +1,18 @@
 package backend.service;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 
 public class UserServiceIntegrationTest {
 
+	@Test
+	public void test()
+	{
+		assertTrue(true);
+	}
 }
