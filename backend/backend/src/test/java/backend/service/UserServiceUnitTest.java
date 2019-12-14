0154228c6f95af7ee52d546afd6bcfe6eac6d4dc
Dusan Stevic <stevicdule@gmail.com>
2019-12-14 20:08:10 +0100

    postavka frontend projekta

--- backend/backend/src/test/java/backend/service/UserServiceUnitTest.java
+++ backend/backend/src/test/java/backend/service/UserServiceUnitTest.java
@@ -2,12 +2,22 @@ package backend.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import backend.repository.UserRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class UserServiceUnitTest {
-
+	@Autowired
+	private UserService userService;
+	
+	@MockBean
+	private UserRepository userRepositoryMocked;
+	
+	
 }
