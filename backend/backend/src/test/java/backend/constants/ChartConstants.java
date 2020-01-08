272188ad99dc0596218025f3c4652cbef3e6671c
Arpad Varga Šomođi <arpad.varga.somodji@gmail.com>
2020-01-08 07:24:03 +0100

    Bugfix at standing tickets, more entitys added to test and main DB

--- backend/backend/src/test/java/backend/constants/ChartConstants.java
+++ backend/backend/src/test/java/backend/constants/ChartConstants.java
@@ -3,11 +3,11 @@ package backend.constants;
 public class ChartConstants {
 	
 	//System Informations
+	public static final int INFO_NUM_EVENTS = 2;
 	public static final int INFO_NUM_ADMIN = 1;
 	public static final int INFO_NUM_USERS = 2;
-	public static final int INFO_NUM_EVENTS = 1;
-	public static final double INFO_ALLTIME_INCOME = 1400.0;
-	public static final int INFO_ALLTIME_TICKETS = 2;
+	public static final double INFO_ALLTIME_INCOME = 2300.0;
+	public static final int INFO_ALLTIME_TICKETS = 5;
 	
 	
 
