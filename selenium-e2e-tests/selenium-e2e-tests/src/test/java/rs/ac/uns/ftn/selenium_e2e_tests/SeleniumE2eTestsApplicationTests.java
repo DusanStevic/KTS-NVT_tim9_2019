634601059b43388e870e906f6c1495c5b68ded9f
Dusan Stevic <stevicdule@gmail.com>
2020-01-22 15:00:25 +0100

    selenium tests setup

---
+++ selenium-e2e-tests/selenium-e2e-tests/src/test/java/rs/ac/uns/ftn/selenium_e2e_tests/SeleniumE2eTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.selenium_e2e_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumE2eTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
