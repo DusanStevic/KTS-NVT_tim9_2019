3895353dc0883d3f0e41d71d4adc6230671a84b4
Dusan Stevic <stevicdule@gmail.com>
2020-01-24 13:07:05 +0100

    toastr JS library

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSliderModule} from '@angular/material/slider';
+import { ToastrModule } from 'ngx-toastr';
+import { MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,13 @@ import {MatSliderModule} from '@angular/material/slider';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      closeButton: true,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
