d71e3df0d342e8a2e52053cac5cc55cc64172867
Arpad Varga Šomođi <arpad.varga.somodji@gmail.com>
2020-02-05 17:47:26 +0100

    Added userupdate to backend, finished user update.

--- backend/backend/src/main/java/backend/service/SectorService.java
+++ backend/backend/src/main/java/backend/service/SectorService.java
@@ -9,10 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import backend.dto.SectorDTO;
-import backend.dto.SittingSectorDTO;
-import backend.dto.StandingSectorDTO;
-import backend.exceptions.BadRequestException;
 import backend.exceptions.ResourceNotFoundException;
 import backend.model.Sector;
 import backend.model.SittingSector;
