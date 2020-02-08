70c403e93ca39734ef77e93b5fac3f8cc34af037
Marieta Rakos <marieta.rakos@gmail.com>
2020-02-08 09:53:48 +0100

    sitne izmene

--- frontend/src/app/reservation/reservation.module.ts
+++ frontend/src/app/reservation/reservation.module.ts
@@ -6,11 +6,12 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AngularYandexMapsModule } from 'angular8-yandex-maps';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { ViewReservationComponent, ConfirmCancelReservationDialog } from './view-reservation/view-reservation.component';
+import { MakeReservationComponent } from './make-reservation/make-reservation.component';
 
 
 
 @NgModule({
-  declarations: [MyReservationsComponent, ViewReservationComponent, ConfirmCancelReservationDialog],
+  declarations: [MyReservationsComponent, ViewReservationComponent, ConfirmCancelReservationDialog, MakeReservationComponent],
   imports: [
     CommonModule,
     MaterialModule,
